253c3dde3f56592cf5c525c9838ea9a63caffbfe
acailuv <ainvalde@gmail.com>
2019-06-06 20:10:56 +0700

    Completed! TODO #3
    Completed TODO number 3.

--- Meeting02_Shooter/Shooter.java
+++ Meeting02_Shooter/Shooter.java
@@ -24,6 +24,8 @@ import java.awt.event.KeyEvent;
         > OK!!
         > Press space multiple times to shoot indefinitely!
      3. Limit the amount of bullet in the cannon
+        > Roger that.
+        > I made a label to indicate how much bullets the player has at the moment.
      4. Add wind force, with its direction (which impacts acceleration on x-axis and y-axis; use Newton's second law)
      5. Make a shooter game with simple moving target (yes, over-achievers, I need SIMPLE)
 
@@ -40,6 +42,11 @@ class Shooter {
     private Bullet bullet = null;
     private Cannon cannon;
 
+    // bullet magazine system
+    private int maxBullets = 5;
+    private int currentBullets = 5;
+    private String bulletString = "Bullets: " + Integer.toString(currentBullets) + "/" + Integer.toString(maxBullets);
+
     private static final String INSTRUCTION = "Welcome to Cannon Simulation!\n" +
             "\nMove cannon's position = W A S D\n" +
             "Move shooting direction = Left | Right \n" +
@@ -47,6 +54,10 @@ class Shooter {
             "\nThere can only one bullet at a time";
 
     private int cpSize = 230;      // set control panel's width
+
+    private void updateBullets() {
+        bulletString = "Bullets: " + Integer.toString(currentBullets) + "/" + Integer.toString(maxBullets);
+    }
 
     public Shooter() {
         // setup the frame
@@ -64,9 +75,12 @@ class Shooter {
         JTextField bulletVelocity = new JTextField("50");
         bulletVelocity.setBounds(5, 75, cpSize-5, 20);
         bVLabel.setBounds(5, 55, cpSize-5, 20);
+        JLabel bulletsLabel = new JLabel(bulletString);
+        bulletsLabel.setBounds(5, 95, cpSize-5, 20);
         frame.add(instruction);
         frame.add(bVLabel);
         frame.add(bulletVelocity);
+        frame.add(bulletsLabel);
 
         // setup drawing area
         DrawingArea drawingArea = new DrawingArea(frame.getWidth(), frame.getHeight(), cpSize);
@@ -81,9 +95,14 @@ class Shooter {
                 super.keyPressed(e);
                 switch (e.getKeyCode()) {
                     case KeyEvent.VK_SPACE:
-                        frame.requestFocusInWindow();
-                        bullet = new Bullet(cannon.getBarrelWidth() / 2, (int) cannon.getBarrelMouthX(), (int) cannon.getBarrelMouthY(), cannon.getAngle(), Double.parseDouble(bulletVelocity.getText()), drawingArea.getTime());
-                        drawingArea.setBullet(bullet);
+                        if(currentBullets > 0) {
+                            frame.requestFocusInWindow();
+                            bullet = new Bullet(cannon.getBarrelWidth() / 2, (int) cannon.getBarrelMouthX(), (int) cannon.getBarrelMouthY(), cannon.getAngle(), Double.parseDouble(bulletVelocity.getText()), drawingArea.getTime());
+                            drawingArea.setBullet(bullet);
+                            currentBullets--;
+                            updateBullets();
+                            bulletsLabel.setText(bulletString);
+                        }
                         break;
                 }
             }
@@ -95,8 +114,13 @@ class Shooter {
                 super.keyPressed(e);
                 switch (e.getKeyCode()) {
                     case KeyEvent.VK_SPACE:
-                        bullet = new Bullet(cannon.getBarrelWidth() / 2, (int) cannon.getBarrelMouthX(), (int) cannon.getBarrelMouthY(), cannon.getAngle(), Double.parseDouble(bulletVelocity.getText()), drawingArea.getTime());
-                        drawingArea.setBullet(bullet);
+                        if(currentBullets > 0) {
+                            bullet = new Bullet(cannon.getBarrelWidth() / 2, (int) cannon.getBarrelMouthX(), (int) cannon.getBarrelMouthY(), cannon.getAngle(), Double.parseDouble(bulletVelocity.getText()), drawingArea.getTime());
+                            drawingArea.setBullet(bullet);
+                            currentBullets--;
+                            updateBullets();
+                            bulletsLabel.setText(bulletString);
+                        }
                         break;
                     case KeyEvent.VK_LEFT:
                         cannon.rotateLeft();
